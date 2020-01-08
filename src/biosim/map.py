c59aa82f2a9a9dbf13b707ba31494eff8eb43919
Hans Kristian <hanslund@nmbu.no>
2020-01-08 10:32:20 +0100

    added different way to replace whitespaces in the map_string

--- src/biosim/map.py
+++ src/biosim/map.py
@@ -5,26 +5,28 @@ __email__ = 'hans.kristian.lunda@nmbu.no, mikkreks@nmbu.no'
 
 class Map:
     """
-    Creates the coordinates of the map based on multi_line_map_string, and add
-    the corresponding landscape type
+    Creates a dictionary with the coordinates of the map based on
+    multi_line_map_string, and add the corresponding landscape type
     """
 
     def __init__(self, map_string):
         self.map_string = map_string
         self.map_string.splitlines()
-        dictionary = {}
+        self.dictionary = {}
         antall_rader = len(self.map_string)
 
-        for i in range(antall_rader):
-            old_value = str(map_string_split[i])
-            new_value = old_value.replace(' ', '')
-            map_string_split[i] = new_value
+        self.map_string = [k.replace(' ', '') for k in self.map_string]
 
-        antall_kolonner = len(str(map_string_split[0]))
+        # for i in range(antall_rader):
+        #     old_value = str(map_string_split[i])
+        #     new_value = old_value.replace(' ', '')
+        #     map_string_split[i] = new_value
+
+        antall_kolonner = len(str(self.map_string_split[0]))
         for i in range(antall_rader):
             for j in range(antall_kolonner):
                 # dictionary[index, sub_index] = item
-                dictionary[i, j] = map_string_split[i][j]
+                self.dictionary[i, j] = self.map_string_split[i][j]
 
         print(dictionary)
 
