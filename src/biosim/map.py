2fde28dc3929ec425cd36b04f05b5c5df22775fa
Mikko <mikkreks@nmbu.no>
2020-01-10 10:40:46 +0100

    Map is now based on a list of cell-objects

--- src/biosim/map.py
+++ src/biosim/map.py
@@ -9,24 +9,26 @@ class Map:
     """
 
     def __init__(self, map_string):
-        self.map_string = map_string
+        """
+        Will create the map as a list, containing objects with cells.
+        Will also create a matrix in order to be able to visualize the map
+        easier later.
+        """
+        # self.population_herbivores_list
         self.map_string_split = map_string.split()
         self.n_rows = len(self.map_string_split)
         self.n_cols = len(str(self.map_string_split[0]))
-        self.coords = []
-        self.landscape_list = []
-        self.landscape = []
-        # self.landscape_matrix = np.zeros([self.n_rows,self.n_cols])
+
+        self.landscape_matrix = np.zeros([self.n_rows, self.n_cols])
+        self.cell_map = []
+
         for i in range(self.n_rows):
             for j in range(self.n_cols):
-                test = (i, j)
-                self.coords.append(test)
-                self.landscape_list.append(self.map_string_split[i][j])
-
-    # self.landscape = list(zip(self.coords, self.landscape_list))
-    # for i in range(len(self.landscape)):
-    #     self.landscape.append({"Fodder:": 0})
-
+                current_cell = Cell((i, j), self.map_string_split[i][j],
+                                       fodder=0, population_herbivores=0)
+                self.cell_map.append(current_cell)
+                map_value = self.convert_landscape(map_string_split[i][j])
+                self.landscape_matrix[i][j] = map_value
 
     def convert_landscape(self, landscape_type):
         if landscape_type == 'O':
@@ -40,21 +42,18 @@ class Map:
         else:
             return 4
 
-    # def create_map(self):
-    #     for i in range(self.n_rows):
-    #         for j in range(self.n_cols):
-    #             test = (i, j)
-    #             map_value = convert_landscape(self.map_string_split[i][j])
-    #             self.landscape_matrix[i][j] = map_value
+    def find(self, coordinate_to_find):
+        for i in range(len(self.cell_map)):
+            if self.cell_map[i].coordinate == coordinate_to_find:
+                return i
 
-    def create_map(self):
-        for i in range(self.n_rows):
-            for j in range(self.n_cols):
-                self.current_map.append(
-                    Cell((self.map_string_split[i], self.coords[j])),)
+    def show_map(self):
+        cmap = mpl.colors.ListedColormap(['royalblue', 'grey', 'khaki', 'honeydew', 'forestgreen'])
+        plt.imshow(self.landscape_matrix, cmap=cmap)
+        plt.show()
 
     def get_map(self):
-        return self.current_map
+        return self.cell_map
 
 
 """
