8494cd9453d7612e97cfe97de2ecfd0b905c41a6
Hans Kristian <hanslund@nmbu.no>
2020-01-16 11:49:37 +0100

    Fixed some typos.

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -1,5 +1,6 @@
 from biosim.fauna import Fauna
 from biosim.Cell import Cell, Jungle, Ocean, Mountain, Savannah, Desert
+
 __author__ = 'Hans Kristian Lunda, Mikko Rekstad'
 __email__ = 'hans.kristian.lunda@nmbu.no, mikkreks@nmbu.no'
 
@@ -9,18 +10,18 @@ This file is used for testing of the class Cell.
 
 
 class TestCell:
-    fauna_list = [{'loc': (3,4),
-      'pop': [{'species': 'herbivore', 'age': 10, 'weight': 15},
-              {'species': 'herbivore', 'age': 5, 'weight': 40},
-              {'species': 'herbivore', 'age': 15, 'weight': 25}]},
-     {'loc': (4,4),
-      'pop': [{'species': 'Herbivore', 'age': 2, 'weight': 60},
-              {'species': 'Herbivore', 'age': 9, 'weight': 30},
-              {'species': 'Herbivore', 'age': 16, 'weight': 14}]},
-     {'loc': (4,4),
-      'pop': [{'species': 'Carnivore', 'age': 3, 'weight': 35},
-              {'species': 'Carnivore', 'age': 5, 'weight': 20},
-              {'species': 'Carnivore', 'age': 8, 'weight': 5}]}]
+    fauna_list = [{'loc': (3, 4),
+                   'pop': [{'species': 'herbivore', 'age': 10, 'weight': 15},
+                           {'species': 'herbivore', 'age': 5, 'weight': 40},
+                           {'species': 'herbivore', 'age': 15, 'weight': 25}]},
+                  {'loc': (4, 4),
+                   'pop': [{'species': 'Herbivore', 'age': 2, 'weight': 60},
+                           {'species': 'Herbivore', 'age': 9, 'weight': 30},
+                           {'species': 'Herbivore', 'age': 16, 'weight': 14}]},
+                  {'loc': (4, 4),
+                   'pop': [{'species': 'Carnivore', 'age': 3, 'weight': 35},
+                           {'species': 'Carnivore', 'age': 5, 'weight': 20},
+                           {'species': 'Carnivore', 'age': 8, 'weight': 5}]}]
 
     test = [{'loc': (10, 10),
              'pop': [{'species': 'herbivore', 'age': 10, 'weight': 15},
@@ -32,14 +33,13 @@ class TestCell:
             ]
 
     test2 = [{'loc': (10, 10),
-             'pop': [{'species': 'herbivore', 'age': 10, 'weight': 15},
-                     {'species': 'herbivore', 'age': 5, 'weight': 100},
-                     {'species': 'herbivore', 'age': 0, 'weight': 6},
-                     {'species': 'herbivore', 'age': 5, 'weight': 45},
-                     {'species': 'herbivore', 'age': 40, 'weight': 43},
-                     {'species': 'herbivore', 'age': 15, 'weight': 25}]}
-            ]
-
+              'pop': [{'species': 'herbivore', 'age': 10, 'weight': 15},
+                      {'species': 'herbivore', 'age': 5, 'weight': 100},
+                      {'species': 'herbivore', 'age': 0, 'weight': 6},
+                      {'species': 'herbivore', 'age': 5, 'weight': 45},
+                      {'species': 'herbivore', 'age': 40, 'weight': 43},
+                      {'species': 'herbivore', 'age': 15, 'weight': 25}]}
+             ]
 
     def test_add_population(self, test=test):
         """
@@ -47,6 +47,7 @@ class TestCell:
         :param test:
         :return:
         """
+        cell_pop = {}
         for item in test:
             cell_pop = item['pop']
 
@@ -59,15 +60,13 @@ class TestCell:
         Test that the correct number of animals are returned
         :return:
         """
+        cell_pop = {}
         for item in test:
             cell_pop = item['pop']
 
         test_cell = Jungle()
         test_cell.add_pop(cell_pop)
         assert test_cell.number_creatures() == 6
-
-
-
 
     def test_get_number_of_carnivores(self):
         """
@@ -85,6 +84,7 @@ class TestCell:
         Test that the correct number of herbivores are returned
         :return:
         """
+        cell_pop = {}
         for item in test:
             cell_pop = item['pop']
 
@@ -95,7 +95,8 @@ class TestCell:
     def test_add_fodder(self):
         """
         Test that the start amount of fodder is correct.
-        Will also test that the test fodder function works propery.
+        Will also test that the test fodder function works properly, and
+        that fodder can be added to a cell.
         :return:
         """
         test_jungle = Jungle()
@@ -104,13 +105,16 @@ class TestCell:
         test_jungle.add_fodder()
         assert test_jungle.fodder == 800.0
 
-        test_savannah = Savanna()
+        test_savannah = Savannah()
         assert test_savannah.fodder == 300.0
         test_savannah.fodder = 0
         test_savannah.add_fodder()
         assert test_savannah.fodder == 30.0
         test_savannah.add_fodder()
         assert test_savannah.fodder == 111.0
+        fodder_test = Cell((2, 2), 'J', fodder=300)
+        amount_of_fodder = fodder_test.get_fodder()
+        assert amount_of_fodder is not None
 
     def test_habitability(self):
         test_jungle = Jungle()
@@ -124,22 +128,22 @@ class TestCell:
         assert not test_ocean.habitable
         assert not test_mountain.habitable
 
-
-    def test_add_fodder(self):
-        """
-        Test that fodder can be added to the cell
-        :return:
-        """
-        fodder_test = Cell((2,2), 'J', fodder=300)
-        amount_of_fodder = fodder_test.get_fodder()
-        assert amount_of_fodder is not None
-        pass
+    # def test_add_fodder(self):
+    #     """
+    #     Test that fodder can be added to the cell
+    #     :return:
+    #     """
+    #     fodder_test = Cell((2, 2), 'J', fodder=300)
+    #     amount_of_fodder = fodder_test.get_fodder()
+    #     assert amount_of_fodder is not None
+    #     # pass
 
     def test_mating_season(self, test=test):
         """
         Test that population can be added to the cell
         :return:
         """
+        cell_pop = {}
         for item in test:
             cell_pop = item['pop']
         test_cell = Jungle()
@@ -163,19 +167,20 @@ class TestCell:
 
     def test_ranked_fitness(self, test=test2):
         """
-        Will test that each creature gest ranked properly. Uses test 2,
+        Will test that each creature gets ranked properly. Uses test 2,
         because there all creatures have different fitness.
         :param test: List of creatures in a population.
         :return:
         """
+        cell_pop = {}
         for item in test:
             cell_pop = item['pop']
         test_cell = Jungle()
         test_cell.add_pop(cell_pop)
         test_cell.ranked_fitness()
-        for i in range(test_cell.number_herbivores()-1):
+        for i in range(test_cell.number_herbivores() - 1):
             fitness1 = test_cell.population_herbivores[i].fitness
-            fitness2 = test_cell.population_herbivores[i+1].fitness
+            fitness2 = test_cell.population_herbivores[i + 1].fitness
             assert fitness1 > fitness2
 
     def test_feed_herbivores(self, test=test):
@@ -186,6 +191,7 @@ class TestCell:
         :param test: dict
         :return:
         """
+        cell_pop = {}
         for item in test:
             cell_pop = item['pop']
         test_cell = Jungle()
@@ -203,7 +209,6 @@ class TestCell:
             # Storing this information for next test.
             weight2.append(test_cell.population_herbivores[i].weight)
 
-
         # Testing that the correct amount of fodder is removed.
         assert test_cell.fodder == 800.0 - 10 * len(weight)
 
@@ -216,9 +221,6 @@ class TestCell:
         for i in range(1, len(weight)):
             assert test_cell.population_herbivores[i].weight == weight2[i]
 
-
-
-
     def test_lose_weight(self, test=test):
         """
         Will test that all creatures lose correct amount of weight.
@@ -226,17 +228,18 @@ class TestCell:
         :param test: dict
         :return:
         """
+        cell_pop = {}
         for item in test:
             cell_pop = item['pop']
         test_cell = Jungle()
         test_cell.add_pop(cell_pop)
-        weight=[]
+        weight = []
         for i in range(test_cell.number_herbivores()):
             weight.append(test_cell.population_herbivores[i].weight)
-        cell.lose_weight()
-        for i in range(test_cell.number_herbivores())
+        test_cell.lose_weight()
+        for i in range(test_cell.number_herbivores()):
             new_weight = test_cell.population_herbivores[i].weight
-            assert  new_weight == weight[i]
+            assert new_weight == weight[i]
 
     def test_remove_pop(self, test=test):
         """
@@ -245,10 +248,3 @@ class TestCell:
         :return:
         """
         pass
-
-
-
-
-
-
-
