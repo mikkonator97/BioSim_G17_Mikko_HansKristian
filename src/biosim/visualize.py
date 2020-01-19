78b3ccefbbed2dad4a25c52282fa6543908bdbf0
Mikko <mikkreks@nmbu.no>
2020-01-19 18:42:00 +0100

    Created skeleton for visualize and subplot setup

--- src/biosim/visualize.py
+++ src/biosim/visualize.py
@@ -6,10 +6,34 @@ class Visulualize:
     This class will create the visual aids, then help us updating them.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, cell_map):
+        plt.subplot(2, 2, 1)
+        plt.title('Map of Rossumøya')
+        plt.subplot(2, 2, 2)
+        plt.title('Total amount of creatures')
+        plt.subplot(2, 2, 3)
+        plt.title('Spread of Herbivores')
+        plt.subplot(2, 2, 4)
+        plt.title('Spread of Carnivores')
+        plt.show()
+
 
 
     def update(self):
+
+        self.update_population_statistics()
+        self.update_herbivores()
+        self.update_carnivores()
+
+    def update_population_statistics(self):
         pass
 
+    def update_herbivores(self):
+        pass
+
+    def update_carnivores(self):
+        pass
+
+
+if __name__ == '__main__':
+    test = Visulualize()
